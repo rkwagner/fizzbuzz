23ebeb246c38a096fa4b563184e3b9d65c26f33d
Ryan Wagner <rkwagner@ucsd.edu>
2016-08-05 16:21:37 -0700

    Simplified output logic.

--- ndigits.py
+++ ndigits.py
@@ -15,10 +15,18 @@ length = int(raw_input("Integer number length:"))
 
 total = []
 
+#create a list of tuples of all permutations (with repetition) of the numbers in the input list.
+#this can be modified by switching to itertools.combination, and removing the user choice to select length.
+#that would make it the sum of all numbers (no repetition) of the same length as the input list,
+#where all numbers are in the list.
+
 total.extend(product(nums, repeat=length))
 
-newt = []
+#convert list of tuples to list of lists, convert to list of strings, then to list of ints.
 
-total = [int(toop) for toop in [''.join(top) for top in [list(tup) for tup in total]]]
+total = [int(''.join(list(tup))) for tup in total]
+#total = [int(toop) for toop in [''.join(top) for top in [list(tup) for tup in total]]]
+
+#get the sum of all list members.
 
 print sum(total)
