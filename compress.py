11e963e0517c51b1114460dbda2ebba578ed32d4
Ryan Wagner <rkwagner@ucsd.edu>
2016-08-07 19:03:23 -0700

    Added semantics to re-decompress data using regex find.

--- compress.py
+++ compress.py
@@ -2,6 +2,7 @@
 #given compression format 2a3b3c=aabbbccc
 #compress arbitrary strings.
 
+import re
 
 def compress(string):
     compressed = ""
@@ -14,4 +15,15 @@ def compress(string):
             count = 1
     return compressed
 
-print compress(raw_input("Enter an input string to compress:"))
+def decompress(string):
+    decompressed = ""
+    nums = re.findall(r'\d+', string)
+    letters = re.findall(r'\D+', string)
+    for i in range(len(nums)):
+        decompressed += int(nums[i])*letters[i]
+
+    print nums, letters, decompressed
+
+compressed = compress(raw_input("Enter an input string to compress:"))
+print "Compressed:", compressed
+decompress(compressed)
