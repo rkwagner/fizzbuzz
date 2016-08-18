6457fcec72345367e36c02907e4cf74f6ff3843b
Ryan Wagner <rkwagner@ucsd.edu>
2016-08-17 18:19:36 -0700

    Modification thoughts to pal.py

--- pal.py
+++ pal.py
@@ -3,6 +3,10 @@ import re, string
 regex = re.compile('[^a-zA-Z]')
 x = raw_input("Enter phrase to check for palindrome: ")
 
+#figure out if the current string is a palindrome using a naive reversal.
+#alternatively, punctuation could be handled by stripping the string
+#of all characters that are not a-zA-Z1-9
+
 retval = regex.sub('', x)
 if retval == retval[::-1]:
     print retval, " is a palindrome.\n" 
