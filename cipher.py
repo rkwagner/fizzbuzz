67eaa0e05ae4b561368bdf7fbdb3f6b4f458d3a8
Ryan Wagner <rkwagner@ucsd.edu>
2016-08-18 23:18:17 -0700

    Issue with bitmasking.

--- cipher.py
+++ cipher.py
@@ -2,3 +2,20 @@
 #strategy: string[i] += (string[i] + i) & 0'b11111111 (255)
 #execution:
 
+#current issue: str and int manipulation, need to learn how to bitmask in python.
+
+fname = 'encode.txt'
+target = open('decode.txt', 'w')
+content = []
+with open(fname) as f:
+    content = f.readlines()
+
+c = []
+for f in content:
+    str = ""
+    for p in range(len(f)):
+        c = ((f[p]+p)&255)
+        str += c
+    c.append(str)
+    target.write(str)
+
